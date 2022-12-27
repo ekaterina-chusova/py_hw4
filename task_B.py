fc12c4a12f76d25debbbc1751751d3d873e853f2
ekaterina-chusova <ekaterina_ch_@mail.ru>
2022-12-27 23:32:34 +0500

    add changes

--- task_B.py
+++ task_B.py
@@ -4,28 +4,55 @@
 # НЕОБЯЗАТЕЛЬНОЕ, ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ:
 # Расширить значение коэффициентов до [-100..100]
 
-import random
+def dict_coef(string):
+    string = string.replace(' ', '').replace('-', '+-').replace('=0', '').split('+')
+    if string[0] =='':
+        string.pop(0)
+    for i in range(len(string)):
+        string[i] = string[i].replace('*', '').split('x')
+        if len(string[i]) < 2:
+            string[i].append('0')
+        if string[i][1] == '':
+            string[i][1] = '1'
+   
+    dict = {int(string[i][1]): int(string[i][0]) for i in range(len(string))}
 
-def create_coefficients(n):
-    coef = {}
-    for i in range(n, -1, -1):
-        if i == n:
-            while True:
-                num = random.randint(-100, 100)
-                if num != 0:
-                    break
-            coef[i] = num
-        else:
-            coef[i] = random.randint(-100, 100)   
-    return(coef)
+    new_dict = {i: 0 for i in range(int(string[0][1]), -1, -1)}  
 
-def print_equation(dict, n):
+    new_dict.update(dict)    
+
+    return (new_dict) 
+
+def result_equation(dict1, dict2):
+    
+    differ_dict = {}
+    if len(dict1) > len(dict2):
+        for i in range(len(dict1)-1, len(dict2)-1, -1):
+            differ_dict[i] = 0
+        dict2 = {**differ_dict, **dict2}
+    else:
+        for i in range(len(dict2)-1, len(dict1)-1, -1):
+            differ_dict[i] = 0
+        dict1 = {**differ_dict, **dict1}
+
+    result = {}
+    for i in range(len(dict2)-1, -1, -1):
+        result[i] = dict1[i] + dict2[i]
+
+    return(result)
+
+def print_equation(dict):
 
     str_equation = ''
     
-    for key in range(n, -1, -1):
-        if key == n:
-            str_equation += str(dict[key]) + f'*x^{key}'        
+    for key in range(len(dict)-1, -1, -1):
+        if key == len(dict)-1:
+            if dict[key] == 1: 
+                str_equation += f'x^{key}'
+            elif dict[key] == -1:
+                str_equation += f'- x^{key}'
+            else:
+                str_equation += str(dict[key]) + f'*x^{key}'
         elif dict[key] == 0:
             str_equation = str_equation
         elif dict[key] == 1 and key != 0: 
@@ -40,40 +67,18 @@ def print_equation(dict, n):
     str_equation = str_equation.replace('+ -', '- ') + ' = 0'        
     return(str_equation)
 
-def result_equation(dict1, n1, dict2, n2):
-    
-    differ_dict = {}
-    if n1 > n2:
-        for i in range(n1, n2, -1):
-            differ_dict[i] = 0
-        dict2 = {**differ_dict, **dict2}
-        count = n1
-    else:
-        for i in range(n2, n1, -1):
-            differ_dict[i] = 0
-        dict1 = {**differ_dict, **dict1}
-        count = n2
-    
-    result = {}
-    for i in range(count, -1, -1):
-        result[i] = dict1[i] + dict2[i]
+string1 = open("homework/hw4/eq1.txt")
+equation1 = string1.read()
+string1.close()
 
-    return(result, count) 
+string2 = open("homework/hw4/eq2.txt")
+equation2 = string2.read()
+string2.close()
 
-k1 = int(input("Введите значение натуральной степени первого многочлена: "))
-k2 = int(input("Введите значение натуральной степени второго многочлена: "))
-
-coefficient1 = create_coefficients(k1)
-# print(coefficient1)
-print(print_equation(coefficient1, k1))
-
-coefficient2 = create_coefficients(k2)
-# print(coefficient2)
-print(print_equation(coefficient2, k2)) 
-
-new_equation, count = result_equation(coefficient1, k1, coefficient2, k2)  
-# print(new_equation)
+print(equation1)
+my_dict1 = dict_coef(equation1)
+print(equation2)
+my_dict2 = dict_coef(equation2)
 
 print('Сумма многочленов равна:')  
-print(print_equation(new_equation, count))
-
+print(print_equation(result_equation(my_dict1, my_dict2)))
